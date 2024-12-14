12117dfd05c363a339dded0ebf90fc802b54abd9
Einar Tryggvi Leifsson <einartryggvi@gmail.com>
2024-12-14 11:24:21 +0000

    Day 14 - part 1

---
+++ day14.py
@@ -0,0 +1,60 @@
+
+from collections import defaultdict
+import re
+
+
+def parse(s):
+    m = re.match(r'.*p=([0-9]+),([0-9]+) v=([0-9\-]+),([0-9\-]+).*', s)
+    return (int(m.group(1)), int(m.group(2))), (int(m.group(3)), int(m.group(4)))
+
+
+def quadrant(cx, cy, x, y):
+    if (x > cx and y > cy):
+        return 1
+    elif (x < cx and y > cy):
+        return 2
+    elif (x < cx and y < cy):
+        return 3
+    elif (x > cx and y < cy):
+        return 4
+
+
+def part1():
+    with open('./inputs/day14.txt') as file:
+        maxX = 101
+        maxY = 103
+        output = 1
+        counts = defaultdict(int)
+        robots = []
+        for line in file:
+            p, v = parse(line.strip())
+            robots.append((p, v))
+
+        for i in range(100):
+            counts.clear()
+            for i, (p, v) in enumerate(robots):
+                (x, y) = p
+                (vx, vy) = v
+                nx, ny = x + vx, y + vy
+                if nx < 0:
+                    nx = maxX + nx
+                if ny < 0:
+                    ny = maxY + ny
+                if nx >= maxX:
+                    nx = nx - maxX
+                if ny >= maxY:
+                    ny = ny - maxY
+                counts[nx, ny] += 1
+                robots[i] = ((nx, ny), v)
+        center = (maxX//2, maxY//2)
+        quadrant_counts = defaultdict(int)
+        for (x, y), c in counts.items():
+            q = quadrant(center[0], center[1], x, y)
+            if q:
+                quadrant_counts[q] += c
+        for c in quadrant_counts.values():
+            output *= c
+        return output
+
+
+print("Part 1: ", part1())
